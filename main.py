b1427781e0eb08b127ec9befb71defcea7d1ae8e
Flexenguy <113939546+Flexenguy@users.noreply.github.com>
2022-09-19 22:08:16 +0300

    Add files via upload
    DISCORD Flexen.js#8309
    pip install requests
    and next import shts

---
+++ main.py
@@ -0,0 +1,37 @@
+from sqlite3 import Time
+from colorama import init, Fore
+import os
+from os import path
+import time
+from urllib import request
+import subprocess
+import shutil
+import stat
+import random
+os.system("cls")
+
+os.system("title Flexen.js#8309")
+
+print("████████╗██╗░░░██╗██████╗░██████╗░░█████╗░  ░██████╗██████╗░░█████╗░░█████╗░███████╗███████╗██████╗░")
+print("╚══██╔══╝██║░░░██║██╔══██╗██╔══██╗██╔══██╗  ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝██╔══██╗")
+print("░░░██║░░░██║░░░██║██████╔╝██████╦╝██║░░██║  ╚█████╗░██████╔╝██║░░██║██║░░██║█████╗░░█████╗░░██████╔╝")
+print("░░░██║░░░██║░░░██║██╔══██╗██╔══██╗██║░░██║  ░╚═══██╗██╔═══╝░██║░░██║██║░░██║██╔══╝░░██╔══╝░░██╔══██╗")
+print("░░░██║░░░╚██████╔╝██║░░██║██████╦╝╚█████╔╝  ██████╔╝██║░░░░░╚█████╔╝╚█████╔╝██║░░░░░███████╗██║░░██║")
+print("░░░╚═╝░░░░╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░  ╚═════╝░╚═╝░░░░░░╚════╝░░╚════╝░╚═╝░░░░░╚══════╝╚═╝░░╚═╝")
+
+print("1) Spoof Fivem")
+print("2) Hwid Change")
+print("3) Soon Mac Next Part")
+
+option = int(input(""))
+
+if option == "1":
+    print("")
+    os.system("cls")
+    os.system("python spoof.py")
+elif option == "2":
+    print("")
+    os.system("cls")
+    os.system("python hwid.py")
+else:
+    print("")
